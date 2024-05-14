2d3c2fc809b436875abc4ba2af98b6e9cd6d19ea
Manoranjan Mishra <manoranjan.mishra.mail@gmail.com>
2024-05-14 23:09:35 +0530

    fixed/ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Map from "./Components/Map";
 import Navbar from "./Components/Navbar";
-import {useJsApiLoader } from '@react-google-maps/api';
+import { useJsApiLoader } from '@react-google-maps/api';
 
 import { mapOptions } from "./Components/MapConfiguration";
 function App() {
   const { isLoaded } = useJsApiLoader({
-    id:mapOptions.googleMapsApiKey,
+    id: mapOptions.googleMapsApiKey,
     googleMapsApiKey: mapOptions.googleMapsApiKey
   })
   return (
-  <div className="App">
-    <Navbar/>
-<Map isLoaded={isLoaded}/>
-  </div>
+    <div className="App" style={{
+      height: "100dvh", display: "flex", flexDirection: "column"
+    }}>
+      <Navbar />
+      <Map isLoaded={isLoaded} style={{ flexGrow: 1 }} />
+    </div>
   );
 }
 
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,51 +1,20 @@
-import React from 'react';
-import { GoogleMap } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap } from "@react-google-maps/api";
+
+import img from "./../img.webp";
 
 const Map = (props) => {
   const { isLoaded } = props;
 
   const containerStyle = {
-    width: '100vw',
-    height: '100vh',
-    display: 'flex',
+    width: "100%",
+    height: "100%",
+    display: "flex",
   };
 
   const mapContainerStyle = {
-    width: '100%',
-    height: '100%',
-  };
-
-  const leftDivStyle = {
-    width: '30vw',
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-  };
-
-  const subDivStyle = {
-    flex: 1,
-    backgroundColor: '#f0f0f0',
-    border: '1px solid #ccc',
-    margin: '5px',
-    display: 'flex',
-    flexDirection: 'column',
-  };
-
-  const buttonContainerStyle = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    padding: '10px',
-  };
-
-  const buttonStyle = {
-    margin: '5px',
-    padding: '10px',
-    backgroundColor: '#fff',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    cursor: 'pointer',
+    width: "70%",
+    height: "100%",
   };
 
   const center = {
@@ -53,30 +22,37 @@ const Map = (props) => {
     lng: 85.8245,
   };
 
-  return isLoaded && (
-    <div style={containerStyle}>
-      <div style={mapContainerStyle}>
+  return (
+    isLoaded && (
+      <div style={containerStyle}>
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
           center={center}
           zoom={10}
-        >
-        </GoogleMap>
-      </div>
-      <div style={leftDivStyle}>
-        <div style={subDivStyle}>
-          <div style={buttonContainerStyle}>
-            <button style={buttonStyle}>Button 1</button>
-            <button style={buttonStyle}>Button 2</button>
-            <button style={buttonStyle}>Button 3</button>
-            <button style={buttonStyle}>Button 4</button>
+        ></GoogleMap>
+        <div style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
+          <div style={{ height: "50%", display: "flex", alignItems: "start", gap: "5px", padding: "10px" }}>
+            <button style={{ padding: "10px 15px", flexGrow: 1 }}>Quick</button>
+            <button style={{ padding: "10px 15px", flexGrow: 1 }}>
+              Button 2
+            </button>
+            <button style={{ padding: "10px 15px", flexGrow: 1 }}>
+              Button 3
+            </button>
+            <button style={{ padding: "10px 15px", flexGrow: 1 }}>
+              Statics
+            </button>
+          </div>
+          <div style={{ height: "50%" }}>
+            <img
+              src={img}
+              alt={"logo"}
+              style={{ width: "100%", height: "98%", objectFit: "cover" }}
+            />
           </div>
         </div>
-        <div style={subDivStyle}>
-          {/* Content for the second sub-div */}
-        </div>
       </div>
-    </div>
+    )
   );
 };
 
